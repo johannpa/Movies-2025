ee10b414e48625f88045bb7dd0a82ae047b72a9d
johannpa <johann_paque@hotmail.fr>
2025-03-09 20:36:13 +0100

    We can create a main component and add mutiple components like that

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Card = () => { 
+  return (
+    <div>
+      <h2>Card Component</h2>
+    </div>
+    )
+}
 
 const App = () => {
   return (
-    <h2>Functional Arrow Component</h2>
+    <div>
+
+      <h2>Functional Arrow Component</h2>
+  
+      <Card />
+      <Card />
+      <Card />
+    </div>
   )
 }
 
