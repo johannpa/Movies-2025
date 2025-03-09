bd2b992425416cca283f5de4b6b929e8f082c286
johannpa <johann_paque@hotmail.fr>
2025-03-09 21:10:01 +0100

    Use className

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -8,11 +8,8 @@ const Card = ({ title }) => {
 
 const App = () => {
   return (
-    <div>
-
-      <h2>Functional Arrow Component</h2>
-  
-      <Card title="Star Wars"/>
+    <div className="card-container">
+      <Card title="Star Wars" rating={5} isCool={true}/>
       <Card title="Avatar"/>
       <Card title="The Lion King"/>
     </div>
